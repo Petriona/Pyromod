44e98f4251281c7a6d1531205280351228dc7ac7
Petriona <108653563+Petriona@users.noreply.github.com>
2022-08-16 01:42:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def start(bt, message):
   
   a = await bt.copy_message("HagadmansaBot", message.chat.id, message.id)
   await a.reply("/dd")
-  b = await bt.get_history("HagadmansaBot", 1)
+  b = await bt.get_chat_history("HagadmansaBot", 1)
   await message.delete()
   await message.reply(b.text)
   
