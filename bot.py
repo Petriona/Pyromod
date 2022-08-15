1c57d278b3a03e3a684fd2c59e422d302c2fd9e4
Petriona <108653563+Petriona@users.noreply.github.com>
2022-08-16 01:23:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import os
 from pyromod import listen
 from pyrogram import Client
 
+NAME = "Pyromod"
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 SESSION = os.environ.get("SESSION")
 
 
 bot = Client(
+      name=NAME,
       api_id=API_ID,
       api_hash=API_HASH,
       session_string=SESSION
