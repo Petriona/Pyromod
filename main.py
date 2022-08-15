8a05963d2cf69171af8c0e4bfa0b1b266edc3086
Petriona <108653563+Petriona@users.noreply.github.com>
2022-08-16 01:01:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from bot import bot
 from pyromod import listen
 from pyrogram import Client, filters
 
-@bot.on_message(filters.private & filters.command("start"))
-async def genStr(bt, msg):
+# @bot.on_message(filters.private & filters.command("start"))
+# async def genStr(bt, msg):
   
-  hemlo = await msg.reply('Hello')
-  api = await bot.ask(msg.chat.id, "How are you bruh?")
-  await hemlo.edit(api.text)
+#   hemlo = await msg.reply('Hello')
+#   api = await bot.ask(msg.chat.id, "How are you bruh?")
+#   await hemlo.edit(api.text)
+
+@Client.on_message(filters.me & filters.command(["start"], [".", "!", "/"]))
+async def start(client: Client, message: Message):
+    await message.edit("Hello")
   
 if __name__ == "__main__":
     bot.run()
