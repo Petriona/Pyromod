c589f5a8edc962e626626af2092a93162f2595f6
Petriona <108653563+Petriona@users.noreply.github.com>
2022-08-16 11:08:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import asyncio
+import traceback
 # from pyromod import listen
 from pyrogram import Client, filters
 
@@ -30,6 +32,63 @@ async def start(bot, message):
       print(b.text)
  await message.delete()
  await message.reply(b.text)
+
+@bot.on_message(filters.me & filters.command(["eval"], [".", "!", "/"]))
+async def eval(bot, message):
+      
+    cmd = message.text.split(" ", maxsplit=1)[1]
+
+    await message.edit("Processing ...")
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, client, message)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+
+    final_output = "<b>EVAL</b>: "
+    final_output += f"<code>{cmd}</code>\n\n"
+    final_output += "<b>OUTPUT</b>:\n"
+    final_output += f"<code>{evaluation.strip()}</code> \n"
+
+    if len(final_output) > 4096:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await message.reply_document(
+                document=out_file,
+                caption=cmd[: 4096 // 4 - 1]
+            )
+            await message.delete()
+    else:
+        await message.edit(final_output)
+
+
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {l_}" for l_ in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
 
 if __name__ == "__main__":    
  bot.run()
